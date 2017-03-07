ac5b3f94f4635e72cffb58449142bb15cfc05482
Patrick Neville <Patrick Neville>
2017-03-07 16:27:53 +0000

    adding sample testscript to repository

---
+++ src/test/java/Patrick/Patrick/TestNGSeleniumSimpleSearchExample.java
@@ -0,0 +1,41 @@
+package Patrick.Patrick;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class TestNGSeleniumSimpleSearchExample {
+	private WebDriver driver;
+
+	@BeforeSuite
+	public void initDriver() throws Exception {
+		System.out.println("You are testing in Chrome");
+		System.setProperty("webdrive.chrome.webdriver", "C:/Users/patri/Desktop/chromedriver.ext");
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void searchTestNGInGoogle() {
+		final String searchKey = "TestNG";
+		System.out.println("Search " + searchKey + " in google");
+		driver.navigate().to("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		System.out.println("Enter " + searchKey);
+		element.sendKeys(searchKey);
+		System.out.println("submit");
+		element.submit();
+		
+		
+		System.out.println("Got " + searchKey + " results");
+	}
+
+	
+}
