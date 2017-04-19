1113253441b4b64e4ba3e74ca498a26a64d6b148
PatrickNeville1 <patrick.neville1@mycit.ie>
2017-04-19 21:12:17 +0100

    Update TestNGSeleniumSimpleSearchExample.java

--- src/test/java/Patrick/Patrick/TestNGSeleniumSimpleSearchExample.java
+++ src/test/java/Patrick/Patrick/TestNGSeleniumSimpleSearchExample.java
@@ -37,9 +37,10 @@ public class TestNGSeleniumSimpleSearchExample {
 		
 		System.out.println("Got " + searchKey + " results");
 		
+		driver.Quit();
+		
 	}
 	
-	@AfterSuite
-	Webdriver.Quit()
+	
 	
 }
