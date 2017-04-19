b21efd36992b00d10851601dc7dcf21eabf6ac68
PatrickNeville1 <patrick.neville1@mycit.ie>
2017-04-19 20:41:37 +0100

    Update TestNGSeleniumSimpleSearchExample.java

--- src/test/java/Patrick/Patrick/TestNGSeleniumSimpleSearchExample.java
+++ src/test/java/Patrick/Patrick/TestNGSeleniumSimpleSearchExample.java
@@ -18,8 +18,8 @@ public class TestNGSeleniumSimpleSearchExample {
 	@BeforeSuite
 	public void initDriver() throws Exception {
 		System.out.println("You are testing in Chrome");
-		System.setProperty("webdriver.ie.driver", "C://Users//Administrator//Desktop//IEDriverServer");
-		driver = new InternetExplorerDriver();
+		System.setProperty('webdriver.chrome.driver', 'C://Users//Administrator//Desktop//chromedriver_win32//chromedriver.exe');
+		driver = new ChromeDriver();
 		
 	}
 
