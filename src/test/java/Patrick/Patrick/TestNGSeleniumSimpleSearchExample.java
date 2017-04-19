51c2e57aae732c6ca65d54285c835013b392f58d
PatrickNeville1 <patrick.neville1@mycit.ie>
2017-04-19 21:47:32 +0100

    Update TestNGSeleniumSimpleSearchExample.java

--- src/test/java/Patrick/Patrick/TestNGSeleniumSimpleSearchExample.java
+++ src/test/java/Patrick/Patrick/TestNGSeleniumSimpleSearchExample.java
@@ -47,6 +47,63 @@ public class TestNGSeleniumSimpleSearchExample {
 		elementA.submit();
 		
 		System.out.println("Got " + searchKeyA + " results");
+		
+		
+		final String searchKeyB = "Facebook";
+		System.out.println("Search " + searchKeyB + " in google");
+		driver.navigate().to("http://www.google.com");
+		WebElement elementB = driver.findElement(By.name("q"));
+		System.out.println("Enter " + searchKeyB);
+		elementB.sendKeys(searchKeyB);
+		System.out.println("submit");
+		elementB.submit();
+		
+		System.out.println("Got " + searchKeyB + " results");
+		
+		final String searchKeyC = "Instagram";
+		System.out.println("Search " + searchKeyC + " in google");
+		driver.navigate().to("http://www.google.com");
+		WebElement elementC = driver.findElement(By.name("q"));
+		System.out.println("Enter " + searchKeyC);
+		elementC.sendKeys(searchKeyC);
+		System.out.println("submit");
+		elementC.submit();
+		
+		System.out.println("Got " + searchKeyC + " results");
+		
+		final String searchKeyD = "RTE";
+		System.out.println("Search " + searchKeyD + " in google");
+		driver.navigate().to("http://www.google.com");
+		WebElement elementD = driver.findElement(By.name("q"));
+		System.out.println("Enter " + searchKeyD);
+		elementD.sendKeys(searchKeyD);
+		System.out.println("submit");
+		elementD.submit();
+		
+		System.out.println("Got " + searchKeyD + " results");
+		
+		
+		final String searchKeyE = "MyCIT";
+		System.out.println("Search " + searchKeyE + " in google");
+		driver.navigate().to("http://www.google.com");
+		WebElement elementE = driver.findElement(By.name("q"));
+		System.out.println("Enter " + searchKeyE);
+		elementE.sendKeys(searchKeyE);
+		System.out.println("submit");
+		elementE.submit();
+		
+		System.out.println("Got " + searchKeyE + " results");
+		
+		final String searchKeyF = "BBC";
+		System.out.println("Search " + searchKeyF + " in google");
+		driver.navigate().to("http://www.google.com");
+		WebElement elementF = driver.findElement(By.name("q"));
+		System.out.println("Enter " + searchKeyF);
+		elementF.sendKeys(searchKeyF);
+		System.out.println("submit");
+		elementF.submit();
+		
+		System.out.println("Got " + searchKeyF + " results");
 	}
 	
 	
